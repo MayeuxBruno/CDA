205911394403ffaadaabd8b859d73cf90a3c7519
59011-07-08 <59011-07-08@exchange.ad.afpanet>
2021-10-13 17:25:29 +0200

    13/10/2021 17:25:28,59

---
+++ 01-Bases de données/NoSql/MongoDB/connexion.php
@@ -0,0 +1,8 @@
+<?php
+    // connexion a mongoDb
+    $m = new MongoClient();
+    echo "Connexion à la  base de données établie";
+
+    //Selection de la base
+    //$db=$m->"Jarditout";
+    //echo "Base de données Jarditout selectionnée"
