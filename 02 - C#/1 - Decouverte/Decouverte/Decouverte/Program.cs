0fe227f97cdb42810b0584cc5fb2648d8c47b815
MayeuxBruno <mayeux.bruno.mb@gmail.com>
2021-10-25 22:29:37 +0200

    25/10/2021 22:29

--- 02 - C#/1 - Decouverte/Decouverte/Decouverte/Program.cs
+++ 02 - C#/1 - Decouverte/Decouverte/Decouverte/Program.cs
@@ -7,9 +7,30 @@ namespace Decouverte
         static void Main(string[] args)
         {
             //Console.WriteLine("Hello World!");
-            /* Exercice 1 */
+            /* Exercice 1 
             string chaine1;
+            Console.WriteLine("Entrez la premiere chaine : ");
             chaine1 = Console.ReadLine();
+            Console.WriteLine("La chaine saisie est : "+chaine1);
+            Console.ReadLine();*/
+
+            /* Exercice 2 
+            int entier;
+            Console.WriteLine("Entrez un entier : ");
+            entier = Int32.Parse(Console.ReadLine());
+            Console.WriteLine("L'entier saisie est : " + entier);
+            Console.ReadLine();*/
+
+            /* Exercice 3 
+            int entier1;
+            int entier2;
+            Console.WriteLine("Entrez le premier entier : ");
+            entier1 = Int32.Parse(Console.ReadLine());
+            Console.WriteLine("Entrez le deuxième entier : ");
+            entier2 = Int32.Parse(Console.ReadLine());
+            Console.WriteLine("La somme des deux entiers est : " + (entier1 + entier2));
+            Console.WriteLine("Le quotient des deux entiers est : " + (entier1 / entier2));
+            Console.ReadLine();*/
         }
     }
 }
