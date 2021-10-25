77bffd03cb8fe940091fa70b3d59328f3f463de3
MayeuxBruno <mayeux.bruno.mb@gmail.com>
2021-10-25 17:19:42 +0200

    25/10/2021 17:07

---
+++ 02 - C#/1 - Decouverte/Decouverte/Decouverte/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Decouverte
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
