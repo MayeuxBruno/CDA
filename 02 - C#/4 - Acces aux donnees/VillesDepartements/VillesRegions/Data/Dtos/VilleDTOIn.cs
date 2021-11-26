432b5703501609f3aa4a659afd9a54a86600efa4
MayeuxBruno <mayeux.bruno.mb@gmail.com>
2021-11-26 11:00:23 +0100

    26/11/2021 11:00:20,67

---
+++ 02 - C#/4 - Acces aux donnees/VillesDepartements/VillesRegions/Data/Dtos/VilleDTOIn.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace VillesRegions.Data.Dtos
+{
+    public class VilleDTOIn
+    {
+    }
+}
