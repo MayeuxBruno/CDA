744b1e478704e601b0cfd0c6a65f74684ab83986
MayeuxBruno <mayeux.bruno.mb@gmail.com>
2021-12-21 12:32:25 +0100

    midi

--- 05 - AppliWeb/Web Dynamique/JavaScript/1 - Menu Vertical/script.js
+++ 05 - AppliWeb/Web Dynamique/JavaScript/1 - Menu Vertical/script.js
@@ -12,9 +12,16 @@ function AffSousMenu (e)
     {
         listesousmenu[i].style.display="none";
     }
+    var listelogo=document.getElementsByClassName("logomenu")
+    for (let i=0;i<listelogo.length;i++)
+    {
+        listelogo[i].classList.remove("vert");
+    }
     var sousmenu=e.target.parentNode.getElementsByClassName("sousmenu")[0];
     if (sousmenu!=null)
     {
         sousmenu.style.display="block";
     }
+    var logo=e.target.getElementsByTagName("i")[0];
+    logo.classList.add("vert");
 }
